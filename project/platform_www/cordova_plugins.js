6aef26e09c30337427fe933ea4b8cc7d4629b006
Wei Wei <windmaple@gmail.com>
2016-06-01 14:58:42 +0800

    add all iOS files

---
+++ project/platform_www/cordova_plugins.js
@@ -0,0 +1,47 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
+        "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
+        "pluginId": "cordova-plugin-wkwebview-engine",
+        "clobbers": [
+            "cordova.exec"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-meteor-webapp/www/webapp_local_server.js",
+        "id": "cordova-plugin-meteor-webapp.WebAppLocalServer",
+        "pluginId": "cordova-plugin-meteor-webapp",
+        "merges": [
+            "WebAppLocalServer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-crosswalk-webview": "1.6.0",
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-wkwebview-engine": "1.0.2",
+    "cordova-plugin-meteor-webapp": "1.3.0",
+    "cordova-plugin-statusbar": "2.1.2",
+    "cordova-plugin-splashscreen": "3.2.1"
+}
+// BOTTOM OF METADATA
+});
